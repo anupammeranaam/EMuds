061cbefce383e41f37c0b3ee7ab2fb8864f4ddd7
anupammeranaam <54675600+anupammeranaam@users.noreply.github.com>
2019-11-01 16:15:35 +0530

    Create signUp.java

---
+++ signUp.java
@@ -0,0 +1,43 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+
+public class signUp {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					signUp window = new signUp();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public signUp() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 500);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
